69ee010fa0a64f2b62948d800d2ffdcd478374b5
Fuerte 159753 <alejavi568@gmail.com>
2023-11-23 22:46:34 -0600

    perfil

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -11,13 +11,23 @@ import { HttpClient } from '@angular/common/http';
 export class PerfilPage implements OnInit {
   //variables
   clienteId: string = '';
+  clienteData: any = {};
 
   constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       this.clienteId = params['idCliente'];
+      this.obtenerDatosCliente();
     });
   }
 
+  obtenerDatosCliente() {
+    this.http.get(`http://localhost/perfil.php?idCliente=${this.clienteId}`)
+      .subscribe((data: any) => {
+        this.clienteData = data; // Almacenar datos
+      }, (error) => {
+        console.error('Error al obtener los datos del cliente:', error);
+      });
+  }
 }
